878de4883a2392397626e3715763ba7b39854469
Aaryan Dubey <aaryandubey19@gmail.com>
2026-04-16 09:13:56 -0400

    Phase 4: Basic commit_create (tree + message only)

--- commit.c
+++ commit.c
@@ -193,9 +193,19 @@ int head_update(const ObjectID *new_commit) {
 //   - head_update       : moves the branch pointer to your new commit
 //
 // Returns 0 on success, -1 on error.
-int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+int commit_create(const char *message, ObjectID *commit_id) {
+    ObjectID tree_id;
+
+    if (tree_from_index(&tree_id) != 0) return -1;
+
+    char tree_hex[65];
+    hash_to_hex(&tree_id, tree_hex);
+
+    char buffer[1024];
+    int len = sprintf(buffer, "tree %s\n\n%s\n", tree_hex, message);
+
+    if (object_write(OBJ_COMMIT, buffer, len, commit_id) != 0) return -1;
+
+    return 0;
 }
+
